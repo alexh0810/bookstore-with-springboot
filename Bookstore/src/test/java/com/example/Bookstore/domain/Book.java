e2639740a257c7ff23441ded31735b5be81b1421
alexh0810 <71229043+alexh0810@users.noreply.github.com>
2021-09-06 10:38:23 +0300

    Add files via upload

---
+++ Bookstore/src/test/java/com/example/Bookstore/domain/Book.java
@@ -0,0 +1,44 @@
+package com.example.Bookstore.domain;
+
+public class Book {
+	
+	public String title; 
+	public String author; 
+	public int year; 
+	public String isbn; 
+	public int price;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	} 
+	
+	
+	
+
+}
