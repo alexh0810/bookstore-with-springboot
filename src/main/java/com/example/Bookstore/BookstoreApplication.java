bed0b4c91e885580a5c5718f2260c1993201a964
alexh0810 <baohoan081097@gmail.com>
2021-10-25 08:46:58 +0300

    Fixing bugs in User class

--- src/main/java/com/example/Bookstore/BookstoreApplication.java
+++ src/main/java/com/example/Bookstore/BookstoreApplication.java
@@ -35,7 +35,7 @@ public class BookstoreApplication {
 			repository.save(new Book("Ernest Hemingway", "A Farewell to Arms", "1232323-21", 1929, crepository.findByName("History").get(0)));
 			repository.save(new Book("George Orwell", "Animal Farm", "2212343-5", 1945, crepository.findByName("Fiction").get(0)));	
 			
-			User user1 = new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER");
+			User user1 = new User("guest", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "GUEST");
 			User user2 = new User("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN");
 			urepository.save(user1);
 			urepository.save(user2);
