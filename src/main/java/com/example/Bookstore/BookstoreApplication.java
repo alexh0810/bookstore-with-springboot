fdb0318df3961b0c2c3cd0d54699e9dc40f0a113
alexh0810 <baohoan081097@gmail.com>
2021-10-27 18:22:54 +0300

    Adding environment variables

--- src/main/java/com/example/Bookstore/BookstoreApplication.java
+++ src/main/java/com/example/Bookstore/BookstoreApplication.java
@@ -25,26 +25,8 @@ public class BookstoreApplication {
 	}
 
 	@Bean
-	public CommandLineRunner bookDemo(BookRepository repository, CategoryRepository crepository, UserRepository urepository ) {
+	public CommandLineRunner bookDemo(BookRepository repository, CategoryRepository crepository) {
 		return (args) -> {
-			log.info("save a couple of books");
-			crepository.save(new Category("Non-fiction"));
-			crepository.save(new Category("Fiction"));
-			crepository.save(new Category("History"));
-			
-			repository.save(new Book("Ernest Hemingway", "A Farewell to Arms", "1232323-21", 1929, crepository.findByName("History").get(0)));
-			repository.save(new Book("George Orwell", "Animal Farm", "2212343-5", 1945, crepository.findByName("Fiction").get(0)));	
-			
-			User user1 = new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER");
-			User user2 = new User("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN");
-			urepository.save(user1);
-			urepository.save(user2);
-			
-			
-			log.info("fetch all books");
-			for (Book book : repository.findAll()) {
-				log.info(book.toString());
-			}
 		};
 	}
 }
