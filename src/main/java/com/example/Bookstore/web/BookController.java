696f1b1426b92c8a8cbd0484fa2505bf28bfead6
alexh0810 <baohoan081097@gmail.com>
2021-10-28 20:48:49 +0300

    Fixing bugs with Status enum

--- src/main/java/com/example/Bookstore/web/BookController.java
+++ src/main/java/com/example/Bookstore/web/BookController.java
@@ -19,67 +19,69 @@ import com.example.Bookstore.domain.CategoryRepository;
 @Controller
 public class BookController {
 
-	// Read functionality: Show all books
+    // Read functionality: Show all books
 
-	@Autowired
-	private BookRepository repository;
+    @Autowired
+    private BookRepository repository;
 
-	@Autowired
-	private CategoryRepository crepository;
+    @Autowired
+    private CategoryRepository crepository;
 
-	@RequestMapping(value = { "/", "/booklist" })
-	public String studentList(Model model) {
-		model.addAttribute("books", repository.findAll());
-		return "booklist";
-	}
+    @RequestMapping(value = {"/", "/booklist"})
+    public String studentList(Model model) {
+        model.addAttribute("books", repository.findAll());
+        return "booklist";
+    }
 
-	// RESTful service to get all books
-	@RequestMapping(value = "/books", method = RequestMethod.GET)
-	public @ResponseBody List<Book> bookListRest() {
-		return (List<Book>) repository.findAll();
-	}
+    // RESTful service to get all books
+    @RequestMapping(value = "/books", method = RequestMethod.GET)
+    public @ResponseBody
+    List<Book> bookListRest() {
+        return (List<Book>) repository.findAll();
+    }
 
-	// RESTful service to get book by id
-	@RequestMapping(value = "/book/{id}", method = RequestMethod.GET)
-	public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long bookId) {
-		return repository.findById(bookId);
-	}
+    // RESTful service to get book by id
+    @RequestMapping(value = "/book/{id}", method = RequestMethod.GET)
+    public @ResponseBody
+    Optional<Book> findBookRest(@PathVariable("id") Long bookId) {
+        return repository.findById(bookId);
+    }
 
-	// Create functionality: Adding new book
+    // Create functionality: Adding new book
 
-	@RequestMapping(value = "/add")
-	public String addBook(Model model) {
-		model.addAttribute("book", new Book());
-		model.addAttribute("categories", crepository.findAll());
-		return "addbook";
+    @RequestMapping(value = "/add")
+    public String addBook(Model model) {
+        model.addAttribute("book", new Book());
+        model.addAttribute("categories", crepository.findAll());
+        return "addbook";
 
-	}
+    }
 
-	// Saving a book
+    // Saving a book
 
-	@RequestMapping(value = "/save", method = RequestMethod.POST)
-	public String save(Book book) {
-		repository.save(book);
-		return "redirect:booklist";
-	}
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String save(Book book) {
+        repository.save(book);
+        return "redirect:booklist";
+    }
 
-	// Delete functionality: Delete a book
-	@PreAuthorize("hasAuthority('ADMIN')")
-	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
-	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
-		repository.deleteById(bookId);
-		return "redirect:../booklist";
+    // Delete functionality: Delete a book
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+        repository.deleteById(bookId);
+        return "redirect:../booklist";
 
-	}
+    }
 
-	// Update functionality: Updating a book
+    // Update functionality: Updating a book
 
-	@RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
-	public String editBook(@PathVariable("id") Long bookId, Model model) {
-		model.addAttribute("book", repository.findById(bookId));
-		model.addAttribute("categories", crepository.findAll());
-		return "edit";
+    @RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+    public String editBook(@PathVariable("id") Long bookId, Model model) {
+        model.addAttribute("book", repository.findById(bookId));
+        model.addAttribute("categories", crepository.findAll());
+        return "edit";
 
-	}
+    }
 
 }
